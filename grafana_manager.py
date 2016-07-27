81f8f8c2dd4b8fa914003a9662b0364a0ed70adc
Ryan January <ryan.january@egov.com>
2016-07-26 20:47:17 -0500

    Finished backup, restore, and instance to instance syncronization.

--- grafana_manager.py
+++ grafana_manager.py
@@ -1,9 +1,3 @@
-'''
-http://docs.grafana.org/reference/http_api/
-
-- todo backup rotation - file folders for date/time.  Keep certain number of days
-
-'''
 import os, click, json, requests, datetime
 from pprint import pprint
 
@@ -31,10 +25,6 @@ def get_dashboards_in_backup(backupfolder):
         with open(os.path.join(backupfolder, 'backup_results.json')) as bf:
             jbf = json.load(bf)
             return {'src': jbf['host'], 'dashboards': jbf['backup_results']}
-            # print '{} (SRC:{})'.format(dir, jbf['host'])
-            # for dash in jbf['backup_results']:
-            #     print 'ID: {} URI: {} VERSION: {} STATUS: {}'.format(dash['id'], dash['uri'], dash['version'],
-            #                                                          dash['status_code'])
     except IOError as e:
         print 'backup_results not found'
         pass
@@ -66,12 +56,10 @@ def backup_dash(host_alias,dashboard_uri,backup_path):
         if response.status_code == 200:
             dash_source=response.json()
             backup_record['version'] = dash_source['meta']['version']
-            backup_record['original_title']=dash_source['dashboard']['originalTitle']
             backup_record['id']=dash_source['dashboard']['id']
             fd.write(response.content)
         else:
             backup_record['version'] = None
-            backup_record['original_title'] = None
             backup_record['id'] = None
             print 'Backup of dashboard {} failed [status code {}]'.format(dashboard_uri,response.status_code)
 
@@ -93,43 +81,43 @@ def delete_dash(host_alias,dashboard_uri):
 
 
 def restore_dash(host_alias,dashboard_uri,backup_directory,replace=False):
-    print 'Restoring Dashboard HA: {} DU: {}, REP: {}'.format(host_alias,dashboard_uri,replace)
+    if dashboard_uri[0:3] == 'db/':
+        dashboard_uri = dashboard_uri[3:]
+    print 'Restoring Dashboard {1} to host {0} from backup {2}.  Replace={3}'.format(
+                                                        host_alias,dashboard_uri,backup_directory,replace)
     host = get_host_info(host_alias)
     existing_dashlist = get_dashboards(host_alias)
     restore_file=os.path.join(backup_directory,dashboard_uri)
     replacement_ok = False                    #initialize replacement variable to err to caution
     request_data=dict()
-    if dashboard_uri[0:3] == 'db/':
-        dashboard_uri = dashboard_uri[3:]
 
     try:
-        with open(restore_file) as bf:          #load dashboard data from backupfile
+        with open(restore_file) as bf:                  #load dashboard data from backupfile
             request_data['dashboard'] = json.load(bf)['dashboard']
+            request_data['dashboard']['id'] = None      #null out id, let grafana handle it
     except IOError as e:
         print 'unable to open file {}'.format(restore_file)
         return False
 
-    if replace:               #grafana doesn't like to reuse an ID if it's been deleted, check if we're good to replace
+    if replace:               #Check if a dashboard of the current title already exists
         for dash in existing_dashlist:
-            if dash['id'] == request_data['dashboard']['id']:
-                if dash['title'] == request_data['dashboard']['title']:
-                    replacement_ok = True
+            if dash['title'] == request_data['dashboard']['title']:
+                replacement_ok = True
 
     if not replacement_ok:
-        request_data['dashboard']['id'] = None
         request_data['overwrite'] = False
     else:
         request_data['overwrite'] = True
 
-    response = requests.put(url='http://{}:{}/api/dashboards/'.format(
+    response = requests.post(url='http://{}:{}/api/dashboards/db'.format(
         host['host'], host['port'], dashboard_uri),
         data=json.dumps(request_data),
-        headers={'authorization': 'Bearer {}'.format(host['auth_token'])}
+        headers={'authorization': 'Bearer {}'.format(host['auth_token']), 'Content-Type': 'application/json'}
         )
     if response.status_code != 200:
-        print response.url
+        print 'Sending data to URL: {} '.format(response.url)
         print 'Restore failed with status code {}'.format(response.status_code)
-        print response.content
+        print 'Response Content: {}'.format(response.content)
         pprint(json.dumps(request_data))
         return False
     else:
@@ -208,7 +196,10 @@ def delete(**kwargs):
 def restore(**kwargs):
 
     if kwargs['frombackup'] == 'last':
-        backup_folder = '20160726T10:21:31'
+        backup_folders = os.listdir(config['app_config']['backup_location']) #get folders in sorted order
+        backup_folders.sort()
+        backup_folder=backup_folders[-1]   #pull last item off the sorted path
+        print 'Using last backup folder of {}'.format(backup_folder)
     else:
         backup_folder = kwargs['frombackup']
 
@@ -235,29 +226,44 @@ def sync(**kwargs):
     dest_host       = get_host_info(kwargs['destination'])
     source_dashlist = get_dashboards(kwargs['source'])
     dest_dashlist   = get_dashboards(kwargs['destination'])
+    request_data    = dict()
 
     for source_dash in source_dashlist:
-
-        get_response = requests.get(url='http://{}:{}/api/dashboards/db/{}'.format(host['host'], host['port'],dashboard_uri),
-                                headers={'authorization': 'Bearer {}'.format(host['auth_token'])}
+        print 'Syncing {}'.format(source_dash['title'])
+        replacement_ok = False  #set replacement_ok negative initially, only replace if we know we can
+        get_response = requests.get(url='http://{}:{}/api/dashboards/{}'.format(source_host['host'], source_host['port'],source_dash['uri']),
+                                headers={'authorization': 'Bearer {}'.format(source_host['auth_token'])}
                                 )
+        # print get_response.url
+        # print get_response.content
         if get_response.status_code == 200:
             dash_source=get_response.json()
+            # print response_json
+            # dash_source=json.loads(response_json)
         else:
-            print 'Failed to retrieve dashboard.  Status code {}'.format(get_response.status_code)
-            return
+            print 'Failed to retrieve {} dashboard.  Status code {}'.format(source_dash['uri'],get_response.status_code)
+        request_data['dashboard'] = dash_source['dashboard']
+        request_data['dashboard']['id'] = None  # null out id, let grafana handle it
 
-        put_response = requests.put(url='http://{}:{}/api/dashboards/db/{}'.format(
-            dest_host['host'], dest_host['port'], source_dash),
+        for dest_dash in dest_dashlist:
+            if dest_dash['title'] == source_dash['title']:
+                replacement_ok = True
+
+        if not replacement_ok:
+            request_data['overwrite'] = False
+        else:
+            request_data['overwrite'] = True
+
+        response = requests.post(url='http://{}:{}/api/dashboards/db'.format(
+            dest_host['host'], dest_host['port']),
             data=json.dumps(request_data),
-            headers={'authorization': 'Bearer {}'.format(host['auth_token'])}
+            headers={'authorization': 'Bearer {}'.format(dest_host['auth_token']), 'Content-Type': 'application/json'}
         )
-        if put_response.status_code != 200:
-            print put_response.url
-            print 'Restore failed with status code {}'.format(put_response.status_code)
-            print response.content
+        if response.status_code != 200:
+            print 'Sending data to URL: {} '.format(response.url)
+            print 'Restore failed with status code {}'.format(response.status_code)
+            print 'Response Content: {}'.format(response.content)
             pprint(json.dumps(request_data))
-            return False
 
 
 @manage.command()
